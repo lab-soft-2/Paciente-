8d5da9750f6bd32cf681c5660f1a890e044dbff0
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-03-24 13:59:02 -0300

    test

--- src/core/components/patient/controller.ts
+++ src/core/components/patient/controller.ts
@@ -18,12 +18,13 @@ export class PatientController {
         const repository = new PatientRepository(getRepository(Patient))
 
         // const patient = new Patient('testeRota@foi.com','testeRota@foi.com','testeRota@foi.com');
-        const patient = new Patient(email,password,name);
-        repository.create(patient)
+        // const patient = new Patient(email,password,name);
+        // repository.create(patient)
 
         return res
             .status(200)
-            .json({ message: "User created successfully", patient });
+            // .json({ message: "User created successfully", patient });
+            .json({ message: "User created successfully"});
     }
 
 }
