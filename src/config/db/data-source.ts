1164b493153c2e8b2035b82eab7fc4b48f187138
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-03-23 22:46:28 -0300

    test

--- src/config/db/data-source.ts
+++ src/config/db/data-source.ts
@@ -2,7 +2,7 @@ import {DataSource, EntityTarget, ObjectLiteral, Repository} from 'typeorm'
 import { env } from '../env'
 
 export const MainDataSource = new DataSource({
-	host: process.env.TYPEORM_HOST ?? '127.0.0.1',
+	host: process.env.TYPEORM_HOST ?? 'localhost',
 	port: Number(process.env.TYPEORM_PORT ?? '5432'),
 	username: process.env.TYPEORM_USERNAME ?? 'postgres',
 	password: process.env.TYPEORM_PASSWORD ?? 'postgres',
