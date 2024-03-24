1a1aa9017d4429e5540791324b58f331b8297ad4
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-03-24 13:44:46 -0300

    fix patient controller

--- src/core/components/patient/controller.ts
+++ src/core/components/patient/controller.ts
@@ -1,25 +1,29 @@
 import { Request, Response } from "express";
-import { Patient } from "./entity";
+import {logger} from '../../../config/logger'
 import { getRepository, MainDataSource } from './../../../config/db/data-source'
+import { Patient } from "./entity";
+import {PatientRepository} from './repository'
 
 export class PatientController {
 
 
     static async signup(req: Request, res: Response) {
-
+        const log = logger({ context: 'App' })
         await MainDataSource.initialize()
         log.info('Database connected successfully!')
+
+        const { name, email, password } = req.body;
+        log.info(req)
+        log.info( req.body)
         const repository = new PatientRepository(getRepository(Patient))
 
-        const user = new Patient();
-        user.name = name
-        user.email = email
-        user.password = password
+        // const patient = new Patient('testeRota@foi.com','testeRota@foi.com','testeRota@foi.com');
+        const patient = new Patient(email,password,name);
         repository.create(patient)
 
         return res
             .status(200)
-            .json({ message: "User created successfully", user });
+            .json({ message: "User created successfully", patient });
     }
 
 }
