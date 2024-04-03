7ecd67885e2676d3b1871dae2d2f373d3ec777ea
Victor Hugo Chimenez <victorhq.inbox@gmail.com>
2024-04-03 11:03:36 -0300

    test: firebase

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 
@@ -10,14 +10,16 @@ import Medico from './pages/medico'
 import ReceitaGuia from './pages/receita&guia'
 import PostarExames from './pages/postarExame';
 import { auth } from './firebase-config'
+import { User } from 'firebase/auth'
 
-console.log("storage", sessionStorage)
+console.log("storage", localStorage)
 const timout = () => new Promise(resolve => setTimeout(resolve, 10_000))
 
 onAuthStateChanged(auth, async (user) => {
+  console.log(user)
   await timout()
   if (user) {
-    const storedToken = sessionStorage.getItem('userToken')
+    const storedToken = localStorage.getItem('userToken')
     user.getIdToken().then((currentToken) => {
       if (storedToken === currentToken) {
         console.log("Acesso permitido")
@@ -31,6 +33,24 @@ onAuthStateChanged(auth, async (user) => {
 })
 
 function App() {
+  const [user, setUser] = useState<User | null >(null);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("Usuário logado:", user);
+        setUser(user);
+      } else {
+        console.log('empty')
+        await timout()
+      }
+    });
+  }, []);
+
+  if (!user) {
+    // Renderize algum tipo de tela de carregamento ou retorne null
+    return <div>Carregando...</div>;
+  }
   return (
     <>
     <Agenda />
