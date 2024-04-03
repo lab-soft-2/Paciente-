0e4cad197bfc07f4511b53168d5ac794d9696af0
Victor Hugo Chimenez <victorhq.inbox@gmail.com>
2024-04-03 10:32:54 -0300

    test: firebase

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 
 import Agenda from './pages/agenda'
 import Cadastro from './pages/cadastro'
@@ -7,6 +9,26 @@ import Exame from './pages/exame'
 import Medico from './pages/medico'
 import ReceitaGuia from './pages/receita&guia'
 import PostarExames from './pages/postarExame';
+import { auth } from './firebase-config'
+
+console.log("storage", sessionStorage)
+const timout = () => new Promise(resolve => setTimeout(resolve, 10_000))
+
+onAuthStateChanged(auth, async (user) => {
+  await timout()
+  if (user) {
+    const storedToken = sessionStorage.getItem('userToken')
+    user.getIdToken().then((currentToken) => {
+      if (storedToken === currentToken) {
+        console.log("Acesso permitido")
+      } else {
+        window.location.href = 'https://auth-web-app-url.com'
+      }
+    });
+  } else {
+    window.location.href = 'https://auth-web-app-url.com'
+  }
+})
 
 function App() {
   return (
