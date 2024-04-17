e905398448009b4ededd77ecafc8266c2c74cb91
Vitor-chng <63964921+Vitor-chng@users.noreply.github.com>
2024-04-17 10:48:51 -0300

    fix local get medic

--- back/src/core/components/patient/controller.ts
+++ back/src/core/components/patient/controller.ts
@@ -297,7 +297,7 @@ export class PatientController {
 
 
         const resp = http.get({
-            hostname: process.env.MEDIC_SERVICE_HOST ?? 'localhost',
+            hostname: process.env.MEDIC_SERVICE_HOST ?? 'localhost/',
             // 3001 é para testes locais
             port: process.env.MEDIC_SERVICE_PORT ?? 3001,
             path: 'medico/user/all',
